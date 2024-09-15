3eca723cfc1ba919beac2edfb2facdc8cddcf7fa
emmak202@gmail.com <emmak202@gmail.com>
2024-09-15 18:53:28 +0200

    configured terraform backend

--- provider.tf
+++ provider.tf
@@ -1,14 +1,23 @@
 terraform {
+  required_version = ">= 1.5"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.65.0"
-    }
+      version = "~> 5.65.0"
+    }    
+  }
+
+  backend "s3" {
+    bucket = "stack-k8s-terraform-backend-001"
+    key = "build/terraform.tfstate"
+    region = "eu-west-2"
+    profile = "emma"
+    
   }
 }
 
-
 provider "aws" {
   # Configuration options  
-  region = "eu-west-1"
+  region = "eu-west-2"
+  
 }
