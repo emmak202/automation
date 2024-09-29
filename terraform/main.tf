2970329d7043c9f895e9d42d882587508605faf8
emmak202@gmail.com <emmak202@gmail.com>
2024-09-29 15:22:20 +0200

    ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source = "./terraform/modules/network"  
+  source = "./modules/network"  
 
 }
 
 module "k8-cluster" {
-  source = "./terraform/modules/k8s"
+  source = "./modules/k8s"
   vpc_id = module.vpc.vpc-id
   vpc_cidr = var.vpc_cidr
   public_subnet_id = module.vpc.public1_id
